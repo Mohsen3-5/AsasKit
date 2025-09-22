cca3cd11d4eae18a7967c4ef80e087d0e050a194
Mohsen Alyousef <mohsen.alyousef@Vault.Insurance>
2025-09-22 18:00:34 +0300

    Add device token mig.

---
+++ asas/modules/Asas.Identity/src/Asas.Identity.Infrastructure/Migrations/20250922144918_AddDeviceToken.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Asas.Identity.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeviceToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DeviceToken",
+                table: "AsasUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeviceToken",
+                table: "AsasUsers");
+        }
+    }
+}
